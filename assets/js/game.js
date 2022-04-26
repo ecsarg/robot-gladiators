4b5a70ab7ec19de100c8b40d14780db17d58e739
Ellie Sargent <elliecsargent@gmail.com>
2022-04-26 14:44:07 -0500

    attempt at fixing issues

--- assets/js/game.js
+++ assets/js/game.js
@@ -3,40 +3,93 @@ var playerHealth = 100;
 var playerAttack = 10;
 var playerMoney = 10;
 
+// You can also log multiple values at once like this
+console.log(playerName, playerAttack, playerHealth);
+
 var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
-// You can also log multiple values at once like this
-console.log(playerName, playerAttack, playerHealth);
-
 console.log(enemyNames.length);
+
+// function to start a new game
+var startGame = function(){
+    // reset player stats
+    playerHealth = 100;
+    playerAttack = 10;
+    playerMoney = 10;
+    // fight each enemy robot by looping over them and fighting them one at a time
 for(var i = 0; i < enemyNames.length; i++) {
+    // if player is still alive, keep fighting
+    if (playerHealth > 0) {
+        // let user know what round they are in, remember that arrays start at 0 so you must add 1
+        window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
+
+        // pick new emey to fight based on the index of the enemyNames array
     var pickedEnemyName = enemyNames[i];
+
+    //reset enemyHealth before starting new fight
     enemyHealth = 50;
     fight(pickedEnemyName);
 }
+else {
+    window.alert("You have lost your robot in battle! Game Over!");
+    break;
+    }
+}
+
+
+// after the loop ends, player is either out of health or enemies to fight, so run the endGame function
+    endGame();
+};
+
+// function to end the entire game
+var endGame = function(){
+    // if player is stil lalive, player wins!
+    if (playerHealth > 0) {
+        window.alert("Great job, you've survived the game! You now have a score of " + playerMoney + ".");
+    }
+    else {
+        window.alert("You've lost your robot in battle.");
+    }
+    window.alert("The game has now ended. Let's see how you did!");
+
+    //ask player if they'd like to play again
+    var playAgainConfirm = window.confirm("Would you like to play again?");
+
+    if (playAgainConfirm) {
+        // restart the game
+        startGame();
+    }
+    else {
+        window.alert("Thank you for playing Robot Gladiatros! Come back soon!");
+    }
+};
 
 var fight = function(enemyName) {
     while (playerHealth > 0 && enemyHealth > 0) {
         // ask player if they'd like to fight or run
         var promptFight = window.prompt('Would you like to FIGHT or SKIP this battle? Enter "FIGHT" or "SKIP" to choose.');
+        
 
         // if player picks "skip" confirm and then stop the loop
-        if (promptFight === "skip || promptFight === "SKIP")
+        if (promptFight === "skip" || promptFight === "SKIP");
         {
             // confirm player wants to skip
-            var confirmSkip = window.confirm("Are you sure you'd like to quit?"");
+            var confirmSkip = window.confirm("Are you sure you'd like to quit?");
 
             // if yes (true), leave fight
             if (confirmSkip) {
                 window.alert(playerName + ' has decided to skip this fight. Goodbye!');
                 //subtract money from playerMoney for skipping
                 playerMoney = playerMoney - 10;
-                console.log("playerMoney", playerMoney)
+                console.log("playerMoney", playerMoney);
                 break;
             }
         }
+
+        // if player pick fight, then fight
+        if (promptFight === "fight" || promptFight === "FIGHT") {
 
         // remove enemy's health by subtracting the amount set in the playerAttack variable
         enemyHealth = enemyHealth - playerAttack;
@@ -88,9 +141,11 @@ var fight = function(enemyName) {
             window.alert(playerName + ' has died!');
             // leave while() loop if player is dead
             break;
-        } else {
+        } 
+        else {
             window.alert(playerName + ' still has ' + playerHealth + ' health left. ');
         }
+    }
     } // end of while loop
 }; // end of fight function loop
     
@@ -109,3 +164,6 @@ var fight = function(enemyName) {
 
 // run fight function to start game
 fight();
+
+// start the game when the page loads
+startGame();
