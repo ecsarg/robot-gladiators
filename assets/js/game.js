142787ef0cc7b897b5f9dee7958ebf1fc2786305
Ellie Sargent <elliecsargent@gmail.com>
2022-04-27 17:48:55 -0500

    random health and attack values

--- assets/js/game.js
+++ assets/js/game.js
@@ -26,17 +26,18 @@ var fight = function(enemyName) {
             if (confirmSkip) {
                 window.alert(playerName + ' has decided to skip this fight. Goodbye!');
                 //subtract money from playerMoney for skipping
-                playerMoney = playerMoney - 10;
+                playerMoney = Math.max(0, playerMoney - 10);
                 console.log("playerMoney", playerMoney);
                 break;
             }
         }
 
-        // if player pick fight, then fight
-        //if (promptFight === "fight" || promptFight === "FIGHT") {
+        // generate random damage value based on player's attack power
+        var damage = randomNumber(playerAttack - 3, playerAttack);
 
-        // remove enemy's health by subtracting the amount set in the playerAttack variable
-        enemyHealth = enemyHealth - playerAttack;
+        enemyHealth = Math.max(0, enemyHealth - damage);
+
+       
         console.log(
             playerName + ' attacked ' + enemyName + '. ' + enemyName + ' now has ' + enemyHealth + ' health remaining.'
         );
@@ -53,8 +54,12 @@ var fight = function(enemyName) {
             window.alert(enemyName + ' still has ' + enemyHealth + ' health left.');
         }
 
-        // remove player's health by subtracting the amount set in the enemyAttack variable
-        playerHealth = playerHealth - enemyAttack;
+        // generate random damage value based on enemy's attack power
+        var damage = randomNumber(enemyAttack - 3, enemyAttack);
+
+        playerHealth = Math.max(0, playerHealth - damage);
+
+
         console.log(
             enemyName + ' attacked ' + playerName + '. ' + playerName + ' now has ' + playerHealth + ' health remaining.'
         );
@@ -88,7 +93,7 @@ for (var i = 0; i < enemyNames.length; i++) {
     var pickedEnemyName = enemyNames[i];
 
     //reset enemyHealth before starting new fight
-    enemyHealth = 50;
+    enemyHealth = randomNumber(40, 60);
 
     // pass the pickedEnemyName variable's value into the fight function, where it will assume the value of the enemyName parameter
     fight(pickedEnemyName);
@@ -192,24 +197,14 @@ var shop = function() {
     }
 };
 
+// function to generate a random numeric value
+var randomNumber = function(min, max) {
+    var value = Math.floor(Math.random() * (max - min + 1) + min);
 
 
-    
-    
-   
+    return value;
+};
 
-
-
-    
-// Game States
-// "WIN" - Player robot has defeated all enemy-robots
-//      * Fight all enemy-robots
-
-// Game States
-// "LOSE" - Player robot's health is zero or less
-
-// run fight function to start game
-fight();
 
 // start the game when the page loads
 startGame();
